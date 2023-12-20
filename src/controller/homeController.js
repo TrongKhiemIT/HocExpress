cd7645fbdc00ebf34a506c6fc47f04913df30f66
TrongKhiemIT <nguyentrongkhiem010117@gmail.com>
2023-12-20 11:21:04 +0700

    Ket noi MySql

---
+++ src/controller/homeController.js
@@ -0,0 +1,14 @@
+
+
+let getHomePage = (req, res) => {
+    return res.render('homepage.ejs');
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('test/aboutpage.ejs');
+}
+
+module.exports = {
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+}
